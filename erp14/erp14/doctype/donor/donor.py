4c789bca816d2511d7c9a62f308fb3b79f56eb2b
ardco <112344657+ardco@users.noreply.github.com>
2023-05-03 09:57:43 +0200

    Update donor.py

--- erp14/erp14/doctype/donor/donor.py
+++ erp14/erp14/doctype/donor/donor.py
@@ -1,18 +1,18 @@
-# # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
-# # For license information, please see license.txt
+# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
 
 
-# from frappe.contacts.address_and_contact import load_address_and_contact
-# from frappe.model.document import Document
+from frappe.contacts.address_and_contact import load_address_and_contact
+from frappe.model.document import Document
 
 
-# class Donor(Document):
-# 	def onload(self):
-# 		"""Load address and contacts in `__onload`"""
-# 		load_address_and_contact(self)
+class Donor(Document):
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
 
-# 	def validate(self):
-# 		from frappe.utils import validate_email_address
+	def validate(self):
+		from frappe.utils import validate_email_address
 
-# 		if self.email:
-# 			validate_email_address(self.email.strip(), True)
+		if self.email:
+			validate_email_address(self.email.strip(), True)
