1a5f6c6627d86ae51e6329623a5534fff6baae05
ardco <112344657+ardco@users.noreply.github.com>
2023-05-03 10:00:25 +0200

    Update member.py

--- erp14/erp14/doctype/member/member.py
+++ erp14/erp14/doctype/member/member.py
@@ -5,9 +5,9 @@
 import frappe
 from frappe import _
 from frappe.contacts.address_and_contact import load_address_and_contact
-from frappe.integrations.utils import get_payment_gateway_controller
-from frappe.model.document import Document
-from frappe.utils import cint, get_link_to_form
+# from frappe.integrations.utils import get_payment_gateway_controller
+# from frappe.model.document import Document
+# from frappe.utils import cint, get_link_to_form
 
 # from erpnext.erp14.doctype.membership_type.membership_type import get_membership_type
 
